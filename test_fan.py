b00d7413cfd5542d628103bbcf0a2eacda4afc7a
angeljean6 <ajrescober@gmail.com>
2026-06-01 01:15:25 +0800

    initialize testfan script and instantiate first fan object with maximum settings

--- test_fan.py
+++ test_fan.py
@@ -4,4 +4,10 @@ def main():
     print("=" * 44)
     print("   🌀 FAN ELECTRICAL ENCAPSULATION TEST 🌀   ")
     print("=" * 44 + "\n")
-    
+
+def test_fan_program():
+    first_fan = Fan()
+    first_fan.set_speed(Fan.FAST)
+    first_fan.set_radius(10.0)
+    first_fan.set_color("yellow")
+    first_fan.set_is_on(True)
