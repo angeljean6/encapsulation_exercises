d7077d5ea05f591aae4c110fe5bce47311178cf3
angeljean6 <ajrescober@gmail.com>
2026-06-01 00:15:41 +0800

    removing error and fixing functions

--- fan.py
+++ fan.py
@@ -3,9 +3,4 @@ class Fan:
     MEDIUM = 2
     FAST = 3
 
-    def __init__(self, speed: int = SLOW, radius: float = 5.0, color: str = "blue", is_on: bool = False):
-        """Constructor to initialize the fan with private attributes."""
-        self.__speed = speed
-        self.__radius = radius
-        self.__color = color
-        self.__is_on = is_on
+    
