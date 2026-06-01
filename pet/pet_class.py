8f6acba2e227df02262a474d8ac9482860391a2c
angeljean6 <ajrescober@gmail.com>
2026-06-01 22:16:32 +0800

    implement constructor to safely encapsulate raw identity parameters

--- pet/pet_class.py
+++ pet/pet_class.py
@@ -1,2 +1,6 @@
 class Pet:
-    pass
+    def __init__(self):
+        """Constructor initializing private fields for encapsulation."""
+        self.__name = ""
+        self.__animal_type = ""
+        self.__age = 0
