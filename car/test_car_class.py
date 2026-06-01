20a74c17b2aac89abf3b804a96c58ccd827cd862
angeljean6 <ajrescober@gmail.com>
2026-06-01 15:17:57 +0800

    assemble main application shell window and apply matrix dark theme styling

---
+++ car/test_car_class.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from car import Car
+
+class CarDashboardGUI:
+    def __init__(self, window: tk.Tk):
+        self.window = window
+        self.window.title("Nitro Telemetry Matrix")
+        self.window.geometry("450x350")
+        self.window.configure(bg="#121212")
